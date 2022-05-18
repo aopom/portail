a1ff2ce05376d0b078a42e27e6e1ceabfb3666b3
Andrea Chavez <andrea.chavez-herrejon@etu.utc.fr>
2022-05-18 20:37:14 +0200

    Api

--- app/Http/Controllers/v1/Mobilizon/Events.php
+++ app/Http/Controllers/v1/Mobilizon/Events.php
@@ -1,6 +1,14 @@
 <?php
 
 namespace App\Http\Controllers\v1\Mobilizon;
+use GuzzleHttp\Client;
 
-use Illuminate\Support\Facades\Http;
+class Events {
+    public function index(Request $request): JsonResponse{
+
+        $client = new GuzzleHttp\Client();
+        $res = $client->request('GET', 'https://mobitest.ppom.me/graphiql');
+        echo $res->getStatusCode();
+    }
+}
 ?>
