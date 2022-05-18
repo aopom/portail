57c246ea92f16a5528922412969e023725cdc220
Andrea Chavez <andrea.chavez-herrejon@etu.utc.fr>
2022-05-18 15:19:17 +0200

    preuve api

--- resources/assets/react/screens/Evenements.js
+++ resources/assets/react/screens/Evenements.js
@@ -9,14 +9,33 @@
 
 
  class EventsCalendar extends React.Component {
+    constructor(props) {
+        super(props);
     
-     render() {
+        this.state = {events: []}; /* Petit preuve pour eviter redux */
+    }
+    componentDidMount() {
+        this.EventList();
+    }
+    EventList() {
+        $.getJSON('https://assos.utc.fr/api/v1/assos')
+          .then(({ results }) => this.setState({ person: results }));
+    }
+
+    render() {
        
-         return (
+        const events = this.state.events.map((item, i) => (
             <div>
-                <h1>Calendar</h1>
+              <h1>{ item.name }</h1>
+              <h2>{ item.image }</h2>
             </div>
-         );
+          ));
+      
+          return (
+            <div id="layout-content" className="layout-content-wrapper">
+              <div className="panel-list">{ events }</div>
+            </div>
+          );
      }
  }
  
