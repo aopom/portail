fe426278ffcbda8b9ac5546af215d45e14388ef0
Andrea Chavez <andrea.chavez-herrejon@etu.utcfr>
2022-05-21 00:16:43 +0200

    api

--- app/Http/Controllers/v1/Mobilizon/Events.php
+++ app/Http/Controllers/v1/Mobilizon/Events.php
@@ -1,18 +1,76 @@
 <?php
 
 namespace App\Http\Controllers\v1\Mobilizon;
-use GuzzleHttp\Client;
 use App\Http\Controllers\v1\Controller;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use GraphQL\Query;
+use GraphQL\Client;
 
 class Events extends Controller{
     
     public function index(Request $request): JsonResponse{
 
-        $client = new GuzzleHttp\Client();
-        $res = $client->request('GET', 'https://mobitest.ppom.me/graphiql');
-        echo $res->getStatusCode();
+        try{
+            $client = new Client(
+                    'https://mobitest.ppom.me/graphiql'
+            );
+        }catch(ConnectExeption $exception){
+            print_r( $exception->getErrorDetails());
+        }
+    
+        $gql = (new Query('searchEvents'))
+            ->setSelectionSet(
+                [
+                (new Query('elements'))
+                    ->setSelectionSet(
+                        [
+                            'id',
+                            'status',
+                            'category',
+                            'title',
+                            'url',
+                            'joinOptions',
+                            'description',
+                            'beginsOn',
+                            'endsOn',
+                            (new Query('physicalAddress'))
+                                ->setSelectionSet(
+                                    [
+                                        'country',
+                                        'street',
+                                        'geom',
+                                        'postalCode',
+                                        'locality',
+                                        'region',
+                                    ]
+                                ),
+                        (new Query('picture'))
+                                ->setSelectionSet(
+                                    [
+                                        'url'
+                                    ]
+                                ),
+                            (new Query ('organizerActor'))
+                                -> setSelectionSet(
+                                    [
+                                        'name'
+                                    ]
+                                )
+                        ]
+                    )
+                ]
+            );
+        try {
+        
+            $results = $client->runQuery($gql, true);
+            return $results;
+        }
+            catch (QueryException $exception) {
+            print_r($exception->getErrorDetails());
+            exit;
+        }
+    
     }
 }
 ?>
