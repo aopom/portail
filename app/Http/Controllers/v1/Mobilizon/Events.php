47842ff4a13d03cf73196fad122d4d78007f30fc
Andrea Chavez <andrea.chavez-herrejon@etu.utcfr>
2022-05-27 19:15:14 +0200

    show asso calendar

--- app/Http/Controllers/v1/Mobilizon/Events.php
+++ app/Http/Controllers/v1/Mobilizon/Events.php
@@ -6,6 +6,8 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use GraphQL\Query;
 use GraphQL\Client;
+use GraphQL\Variable;
+
 
 class Events extends Controller{
     public function __construct()
@@ -79,5 +81,66 @@ class Events extends Controller{
         }
     
     }
+
+    /**
+     * Show a calendar.
+     *
+     * @param Request	$request
+     * @param string 	$shortname
+     * @return JsonResponse
+     */
+    public function show(Request $request, string $shortname): JsonResponse{
+        try{
+            $client = new Client(
+                    'https://mobitest.ppom.me/graphiql'
+            );
+        }catch(ConnectExeption $exception){
+            print_r( $exception->getErrorDetails());
+        }
+    
+        $gql = (new Query('groups'))
+            ->setVariables([new Variable('preferredUsername', 'String')])
+            ->setSelectionSet([
+                (new Query('organizedEvents'))->setSelectionSet([
+                    (new Query('elements'))->setSelectionSet([
+                            'id',
+                            'status',
+                            'category',
+                            'title',
+                            'url',
+                            'joinOptions',
+                            'description',
+                            'beginsOn',
+                            'endsOn',
+                            (new Query('physicalAddress'))->setSelectionSet([
+                                'country',
+                                'street',
+                                'geom',
+                                'postalCode',
+                                'locality',
+                                'region',
+                            ]),
+                            (new Query('picture'))->setSelectionSet([
+                                'url'
+                            ]),
+                            (new Query ('organizerActor'))-> setSelectionSet([
+                                'name'
+                            ])
+                            ])
+                        ])
+                ]
+            );
+        try {
+        
+            $results = $client->runQuery($gql, true, ['preferredUsername' => "bde"] );
+            return response()->json($results->getData()['searchEvents']['elements']);
+
+        }catch (QueryException $exception) {
+            print_r($exception->getErrorDetails());
+            exit;
+        }
+    
+    }
+
 }
 ?>
