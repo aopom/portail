089643ae9a3e9c9378d33ec7dd8236cc1167361c
Andrea Chavez <andrea.chavez-herrejon@etu.utcfr>
2022-05-27 17:55:17 +0200

    calendar by asso init

--- resources/assets/react/components/Calendar/index.js
+++ resources/assets/react/components/Calendar/index.js
@@ -8,7 +8,7 @@
  * @copyright Copyright (c) 2019, SiMDE-UTC
  * @license GNU GPL-3.0
  */
-
+/* *
 import React from 'react';
 import { connect } from 'react-redux';
 import BigCalendar from 'react-big-calendar';
@@ -222,3 +222,63 @@ class Calendar extends React.Component {
 }
 
 export default Calendar;
+*/
+/**
+ * Displays the association calendars.
+ *
+ * @author Andrea Chávez 
+ * @author Emma Falkiewitz
+ */
+
+ import React from 'react';
+ import BigCalendar from 'react-big-calendar';
+ import moment from 'moment'; 
+ import Views from 'react-big-calendar';
+
+ const localizer = BigCalendar .momentLocalizer(moment); 
+
+ let  views = Object.keys(Views).map((k) => Views[k]);
+
+ class Calendar extends React.Component {
+    constructor(props) {
+        super(props);
+		console.log(this.props)
+        this.state = {events: []}; 
+        this.state = {newEvents: []};
+
+    }
+    componentDidMount() {
+        fetch('/api/v1/eventsMobilizon')
+        .then((response) => response.json())
+        .then(eventsList=> {
+
+          this.setState({ events: eventsList });
+          const newEventsList = this.state.events.map(function(item) {
+            return {title: item.title, start : new Date(item.beginsOn), end : new Date(item.endsOn), url: item.url}    
+          });
+
+          this.setState({newEvents: newEventsList});
+
+        });
+    }
+
+    render() {
+          return (
+
+            <div style={{margin:50}}>
+              <h1 style={{marginBottom: 20}}>Calendrier générale des évènements</h1>
+              <BigCalendar 
+                localizer={localizer}
+                events= {this.state.newEvents}
+                step ={60}
+                defaultDate= {new Date()}			
+                style={{ height: 700 }}
+                onSelectEvent={event => window.open(event.url, "_blank")}
+              />
+            </div>
+          );
+     }
+ }
+ 
+ export default Calendar;
+ 
