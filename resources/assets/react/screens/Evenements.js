29ec679156e19c923ce79fb7eabaa3e92cd033cd
Andrea Chavez <andrea.chavez-herrejon@etu.utcfr>
2022-05-29 14:54:37 +0200

    test

--- resources/assets/react/screens/Evenements.js
+++ resources/assets/react/screens/Evenements.js
@@ -53,7 +53,7 @@ class EventsCalendar extends React.Component {
       this.setState({events:[]});
       this.setState({newEvents:[]});
 
-      let eventsAsso =[];
+      const eventsAsso =[]
 
       this.props.assos.map(function(item){
         fetch('/api/v1/eventsMobilizon/'+(item.shortname)
@@ -70,12 +70,12 @@ class EventsCalendar extends React.Component {
           const newEventsList = this.state.events.map(function(item) {
             return {title: item.title, start : new Date(item.beginsOn), end : new Date(item.endsOn), url: item.url}    
           });
+          
           eventsAsso.push({value:newEventsList});          
         });        
       });  
-      
+
       console.log(eventsAsso);
-      this.setState({newEvents: eventsAsso});
 
     }
     render() {
